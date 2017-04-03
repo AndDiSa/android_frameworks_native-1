49864962e31c7fdff6e7e709cd666fd8e80d94d1
AndDiSa <anddisa@googlemail.com>
2017-04-03 18:35:53 +0200

    Revert "Merge remote-tracking branch 'ads/aosp-7.1' into ads-7.1.0-dev"
    This reverts commit a47f5a9d9cfd769854455156e19a66f2713d0f18, reversing
    changes made to 13289eb91d5158194f8fa74fffb71a2fa53de87c.

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -26,7 +26,6 @@
 
 #include <EGL/egl.h>
 
-#include <cutils/iosched_policy.h>
 #include <cutils/log.h>
 #include <cutils/properties.h>
 
@@ -536,7 +535,6 @@ void SurfaceFlinger::init() {
 
     mEventControlThread = new EventControlThread(this);
     mEventControlThread->run("EventControl", PRIORITY_URGENT_DISPLAY);
-    android_set_rt_ioprio(mEventControlThread->getTid(), 1);
 
     // set a fake vsync period if there is no HWComposer
     if (mHwc->initCheck() != NO_ERROR) {
